2aee419c5f437f15885701a7da71f2f0ba2c7b23
Shaun Clarke <shaunclarke43@gmail.com>
2025-12-31 01:56:46 +0000

    jenkins file to buld maven app docker image

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,14 +1,20 @@
 def buildJar() {
-    echo "Building the app"
-    sh 'mvn package'
+     // specifying the directory to run commands 
+    dir('java-maven-app') {
+        echo "Building the app"
+        sh 'mvn package'
+    }
 }
 
 def buildImage() {
-    echo "Building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 192.168.0.104:8083/demo-app:jma-1.3 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.104:8083'
-        sh 'docker push 192.168.0.104:8083/demo-app:jma-1.3'
+    dir('java-maven-app') {
+        echo "Building the docker image"
+        withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            sh 'docker build -t 192.168.0.104:8083/demo-app:jma-1.3 .'
+            sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.104:8083'
+            sh 'docker push 192.168.0.104:8083/demo-app:jma-1.3'
+        }
+        
     }
 }
 
