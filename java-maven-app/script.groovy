71401d31d19d462d28b83ff2bf0c9d3c8b569ebf
Shaun Clarke <shaunclarke43@gmail.com>
2025-12-31 23:40:28 +0000

    added jenkinsfile

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -1,23 +1,3 @@
-def buildJar() {
-     // specifying the directory to run commands 
-    dir('java-maven-app') {
-        echo "Building the app"
-        sh 'mvn package'
-    }
-}
-
-def buildImage() {
-    dir('java-maven-app') {
-        echo "Building the docker image"
-        withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            sh 'docker build -t 192.168.0.104:8083/demo-app:jma-1.3 .'
-            sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.104:8083'
-            sh 'docker push 192.168.0.104:8083/demo-app:jma-1.3'
-        }
-        
-    }
-}
-
 def deployImage() {
     echo "Deploying the app"
 }
