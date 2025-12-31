fb7972f68a4b4bbff46651656f6530148bf96ded
Shaun Clarke <shaunclarke43@gmail.com>
2025-12-31 23:08:13 +0000

    added build image and build jar groovy files

--- jenkins-shared-library/vars/buildImage.groovy
+++ jenkins-shared-library/vars/buildImage.groovy
@@ -1,6 +1,10 @@
 #!/user/bin/env groovy
 
-import com.example.Docker
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
+    }
 }
