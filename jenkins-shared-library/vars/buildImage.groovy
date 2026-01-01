fa9090fd70a88b564e4f5c5fade199c0e8874078
Shaun Clarke <shaunclarke43@gmail.com>
2026-01-01 01:34:19 +0000

    added param

--- jenkins-shared-library/vars/buildImage.groovy
+++ jenkins-shared-library/vars/buildImage.groovy
@@ -1,10 +1,11 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh "docker push $imageName"
     }
 }
+
