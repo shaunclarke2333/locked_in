ee859a5feb06ad379006b937b2183235890f4ee0
Shaun Clarke <shaunclarke43@gmail.com>
2026-01-01 01:02:42 +0000

    updated repo

--- jenkins-shared-library/vars/buildImage.groovy
+++ jenkins-shared-library/vars/buildImage.groovy
@@ -4,7 +4,7 @@ def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 192.168.0.104:8083'
         sh 'docker push nanatwn/demo-app:jma-2.0'
     }
 }
